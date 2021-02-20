0ce0a7b354338551cc21a03575785f162bf45508
HeshamOsama <shadoW@#94>
2021-02-20 21:44:43 +0200

    1- sign up done

--- app/src/main/java/com/scandrug/scandrug/presentation/authentication/LoginFragment.kt
+++ app/src/main/java/com/scandrug/scandrug/presentation/authentication/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.scandrug.scandrug.presentation.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.scandrug.scandrug.R
+import com.scandrug.scandrug.presentation.home.MainActivity
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
@@ -15,5 +18,13 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        login_company_layout.setOnClickListener {
+            Intent(requireActivity(),MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/scandrug/scandrug/presentation/home/MainActivity.kt
+++ app/src/main/java/com/scandrug/scandrug/presentation/home/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.scandrug.scandrug.presentation.home
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -158,4 +159,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityReenter(resultCode: Int, data: Intent?) {
+
+    }
 }
